c50af2c5c39cdab42b5f51f2c7c29f98f09527e0
pradeep <pradeep.swamy@dotkonnekt.com>
2025-09-08 18:32:38 +0530

    test push

---
+++ Object Repository/Page_Sangria Copilot/div_concat(Let, , s get started)_space-y-2.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_concat(Let, , s get started)_space-y-2</name>
+   <tag></tag>
+   <elementGuidId>7ad3b2b4-26ef-4798-9590-0ee36018ab51</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>form.space-y-4 > div.space-y-2</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Password*'])[1]/preceding::div[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>div >> internal:has-text=/^Email ID\*$/</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>e59bcf0f-8dca-4afe-b85c-15a060d26231</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>space-y-2</value>
+      <webElementGuid>c821c726-b38c-4737-9eca-942200847652</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Email ID*</value>
+      <webElementGuid>6868dc7e-62e3-4a53-b11b-f182288546c1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;__variable_518cef __variable_e8ce0c light&quot;]/body[@class=&quot;font-roboto&quot;]/div[@class=&quot;flex min-h-screen flex-col&quot;]/div[@class=&quot;grid flex-grow grid-cols-1 md:grid-cols-2 relative&quot;]/div[@class=&quot;flex flex-col items-center justify-center p-8&quot;]/div[@class=&quot;w-full max-w-[500px] space-y-6&quot;]/form[@class=&quot;space-y-4&quot;]/div[@class=&quot;space-y-2&quot;]</value>
+      <webElementGuid>d337ec76-e02d-4985-92de-439309424a91</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password*'])[1]/preceding::div[1]</value>
+      <webElementGuid>ffca7148-ab51-429b-9b00-c147d97b0f41</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div</value>
+      <webElementGuid>3682607a-c771-4a0a-8714-30a69b0e63a1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Email ID*' or . = 'Email ID*')]</value>
+      <webElementGuid>6eb06404-2b4c-4fc2-9857-7a43476a423f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>66a54aa0-4fdb-427e-a343-18e5f1a1a708</testSuiteGuid>
+   <testCaseLink>
+      <guid>85e0878c-9ff1-4de7-a766-a7034933232b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
